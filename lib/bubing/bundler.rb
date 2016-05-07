aeafc66f680b72f4874bc08d4d879f767769680c
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2016-05-07 22:29:47 +0300

    fixes and working with simple plugins

--- lib/bubing/bundler.rb
+++ lib/bubing/bundler.rb
@@ -1,8 +1,8 @@
 module Bubing
   class DependencyNotFoundError < StandardError
   end
-  class BundlingError < StandardError
 
+  class BundlingError < StandardError
   end
 
   class Bundler
@@ -16,26 +16,17 @@ module Bubing
       @plugins = plugins
       @plugin_dirs = plugin_dirs
       @verbose = verbose
+      @interpreter = interpreter(binary)
     end
 
     def bundle!
-      int = interpreter(@binary)
-      log("Interpreter is #{int}")
-      deps = get_deps(int, @binary)
-      log("#{deps.count} dependencies found")
-      log('Copying...')
-      FileUtils.rm_rf(Dir.glob(File.join(@directory, '*')))
-      FileUtils.mkdir_p(@directory)
-      FileUtils.cp(deps, @directory)
-      FileUtils.cp(@binary, @directory)
-      FileUtils.cp(int, @directory)
+      log("Interpreter is #{@interpreter}")
+      copy_deps(@binary)
+      log('Copying plugins...')
+      log("#{@plugins.count} plugins must be bundled")
+      copy_plugins
       log('Preparing run.sh...')
-      run_file = File.join(@directory, 'run.sh')
-      run = RUN_TEMPLATE % { interpreter: File.basename(int), binary: File.basename(@binary) }
-      File.open(run_file, 'w') do |file|
-        file.write("#!/bin/bash\n")
-        file.write("#{run}\n")
-      end
+      run_file = make_run
 
       FileUtils.chmod('+x', run_file)
       log('Done!')
@@ -58,13 +49,41 @@ module Bubing
       File.absolute_path(PATH_RE.match(lib)[1].strip)
     end
 
-    def get_deps(interpreter, file)
-      trace = `LD_TRACE_LOADED_OBJECTS=1 #{interpreter} #{file}`
+    def get_deps(file)
+      trace = `LD_TRACE_LOADED_OBJECTS=1 #{@interpreter} #{file}`
       trace.split("\n").map(&:strip).select{|row| row.include?('=>')}.map{|dep| extract_path(dep)}
     end
 
     def log(message)
       puts message if @verbose
+    end
+
+    def copy_deps(binary)
+      log("Bundling $#{binary}")
+      deps = get_deps(binary)
+      log("#{deps.count} dependencies found")
+      log('Copying dependencies...')
+      FileUtils.rm_rf(Dir.glob(File.join(@directory, '*')))
+      FileUtils.mkdir_p(@directory)
+      FileUtils.cp(deps, @directory)
+      FileUtils.cp(@binary, @directory)
+      FileUtils.cp(@interpreter, @directory)
+    end
+
+    def make_run
+      run_file = File.join(@directory, 'run.sh')
+      run = RUN_TEMPLATE % {interpreter: File.basename(@interpreter), binary: File.basename(@binary)}
+      File.open(run_file, 'w') do |file|
+        file.write("#!/bin/bash\n")
+        file.write("#{run}\n")
+      end
+      run_file
+    end
+
+    def copy_plugins
+      @plugins.each do |plugin|
+        copy_deps(plugin)
+      end
     end
   end
 end
